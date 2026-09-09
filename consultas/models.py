01eaf2b5d616f29687182f87863ac585157fe953
ncostadev <n4thanael2007@gmail.com>
2026-09-09 16:48:45 -0300

    formularios
    Criando os models e formulários.

--- consultas/models.py
+++ consultas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Consulta(models.Model):
+    paciente = models.CharField(max_length=100)
+    medico = models.CharField(max_length=100)
+    especialidade = models.CharField(max_length=100)
+    data = models.DateField()
+    horario = models.TimeField()
+    tipo = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Consulta de {self.paciente} com {self.medico} em {self.data} às {self.horario}"
+    
--- consultas/views.py
+++ consultas/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Consulta
+from django import forms
 # Create your views here.
+
+class ConsultaForm(forms.ModelForm):
+    class Meta:
+        model = Consulta
+        fields = '__all__'
+
 
 def home_consultas(request):
     return render(request, 'pages/consultar.html')
 
 
 def cadastrar(request):
-    return render(request, 'pages/cadastrar.html')
+    if request.method == 'POST':
+        form = ConsultaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home_consultas')
+    else:
+        form = ConsultaForm()
+    return render(request, 'pages/cadastrar.html', {'form': form})
 
 
 def editar(request):
